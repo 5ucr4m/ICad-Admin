ee36040152359905b18c76775271c66454156a2a
Tiago Cássio <tiagocassio@outlook.com>
2019-06-11 16:45:34 -0300

    Alterações diversas.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, if: :verify_api
 
-  #before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
