9296a028b00ea627cc8f70f12cd571ac407cc9fc
Tiago Cássio <tiagocassio@outlook.com>
2019-07-19 17:22:35 -0300

    Fixes.

--- app/services/home_visit_registration_service.rb
+++ app/services/home_visit_registration_service.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'xml'
+
 module HomeVisitRegistrationService
   include XML
 
--- app/services/vaccination_campaign_service.rb
+++ app/services/vaccination_campaign_service.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'xml'
+
 module VaccinationCampaignService
   include XML
 
